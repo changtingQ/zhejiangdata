298ce4b3d919266a61f2404e29dee22e42ddd00b
changting <a609504009@gmail.com>
2020-08-05 18:44:24 +0800

    add

--- smcs/items.py
+++ smcs/items.py
@@ -9,6 +9,13 @@ import scrapy
 
 
 class SmcsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    site_name = scrapy.Field()
+    license_key = scrapy.Field()
+    top_domain = scrapy.Field()
+    belonging_city = scrapy.Field()
+    wzfzr = scrapy.Field()
+    wzfzrdh = scrapy.Field()
+    wzfzr_sjhm = scrapy.Field()
+    ztfzr = scrapy.Field()
+    ztfzrdh = scrapy.Field()
+    ztfzr_sjhm = scrapy.Field()
