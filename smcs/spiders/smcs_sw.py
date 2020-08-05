8b6148f446f86029887475d64845ce95c7783e68
changting <a609504009@gmail.com>
2020-08-05 17:31:25 +0800

    add

--- smcs/spiders/smcs_sw.py
+++ smcs/spiders/smcs_sw.py
@@ -14,7 +14,6 @@ class SmcsSwSpider(scrapy.Spider):
             "User-Agent": "Mozilla/5.0 (Linux; U; Android 1.5; en-us; sdk Build/CUPCAKE) AppleWebkit/528.5  (KHTML, like Gecko) Version/3.1.2 Mobile Safari/525.20.1",
             "Host": "183.136.190.39:7070",
             "Origin": "http://183.136.190.39:7070",
-            # "Cookie": "JSESSIONID=E84175C060B8B43DC918F6FCA5952173",
             "Referer": "http://183.136.190.39:7070/smcs_sw/system/redirect/redirectHomePage.do?path=tm/icpWebsite"
         }
         yield scrapy.Request(base_url,callback=self.parse, headers=headers, dont_filter=True, cookies=cookies)
