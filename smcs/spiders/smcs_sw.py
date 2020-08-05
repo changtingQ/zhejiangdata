f28b2d2cb7b3dea1c0939f0a30331c33ca2bcf5b
changting <a609504009@gmail.com>
2020-08-05 17:30:45 +0800

    add

--- smcs/spiders/smcs_sw.py
+++ smcs/spiders/smcs_sw.py
@@ -8,7 +8,7 @@ class SmcsSwSpider(scrapy.Spider):
     def start_requests(self):
         base_url = "http://183.136.190.39:7070/smcs_sw/tm/icpWebsite/getPageModel.do"
         cookies = {
-            "Cookie": "JSESSIONID=E84175C060B8B43DC918F6FCA5952173"
+            "JSESSIONID": "E84175C060B8B43DC918F6FCA5952173"
         }
         headers = {
             "User-Agent": "Mozilla/5.0 (Linux; U; Android 1.5; en-us; sdk Build/CUPCAKE) AppleWebkit/528.5  (KHTML, like Gecko) Version/3.1.2 Mobile Safari/525.20.1",
